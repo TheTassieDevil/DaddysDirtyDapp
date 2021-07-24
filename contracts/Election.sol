953bf6ff8f62e7ea6a855a6181f2610ddd391bb8
TaylorWrightSP <wrighttaylor67@gmail.com>
2021-07-23 21:14:42 -0500

    Unboxed truffle pets box and began work on Election contract

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.6;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+    constructor(Election) {
+        candidate = "Candidate 1";
+    }
+}
