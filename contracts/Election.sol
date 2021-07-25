e22609fa6bfa3caece6e49b3a4b82491c7300a04
TaylorWrightSP <wrighttaylor67@gmail.com>
2021-07-25 12:28:06 -0500

    Ran and created tests (all passing). Modified constructor syntax to be version appropriate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity >=0.4.22;
 
 contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
     // Read/write candidate
-    string public candidate;
+    mapping(uint => Candidate) public candidates;
+
+        // Store Candidates Count
+    uint public candidatesCount;
 
     // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
